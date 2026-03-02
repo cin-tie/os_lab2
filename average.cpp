35ff1fd8212c5674f5349b4640605a1c24aafd93
cintie-windows <csizegod@gmail.com>
2026-03-03 00:38:05 +0300

    Average thread function writed

--- average.cpp
+++ average.cpp
@@ -2,3 +2,18 @@
 #include <iostream>
 
 using namespace std;
+
+DWORD WINAPI average(LPVOID lpParam){
+	ThreadData* data = (ThreadData*)lpParam;
+	int sum = 0;
+
+	for(int i = 0; i < data->size; ++i){
+		sum += data->arr[i];
+		Sleep(12);
+	}
+
+	data->average = (double)sum / data->size;
+	cout << "Average value: " << data->average << endl;
+
+	return 0;
+}
